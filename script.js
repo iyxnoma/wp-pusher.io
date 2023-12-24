c8d730bc334bdbb54a9663db7b82becd261d1fac
Joanna <114005762+iyxnoma@users.noreply.github.com>
2023-12-24 13:44:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerMenu = document.getElementById('menu-icon');
+    const menuItems = document.querySelector('.menu-items');
+  
+    hamburgerMenu.addEventListener('click', () => {
+      menuItems.classList.toggle('show');
+    });
+  });
+  
